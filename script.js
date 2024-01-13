54cfd94f09b9f6a5b58e5ae48c9f14394d1c38f4
MonBort <122515805+MonBort@users.noreply.github.com>
2024-01-14 00:48:53 +0800

    minecraft  text

--- script.js
+++ script.js
@@ -818,6 +818,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let time = document.getElementById("time");
   let frenzy = document.getElementById("frenzycount");
   let gameTitle = document.getElementById("Title");
+  let text = document.getElementById("text");
   let ingame = false;
 
   scoreboard.style.visibility = "hidden";
@@ -828,6 +829,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   play.addEventListener("click", () => {
     gameTitle.classList.add("hide");
+    text.style.visibility = "hidden";
     desc.style.visibility = "hidden";
     scoreboard.style.visibility = "visible";
     expBoard.style.visibility = "visible";
